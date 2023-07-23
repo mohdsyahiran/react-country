a096a6e6909dc32f409a40e074d46e0dce077a6d
mohdsyahiran <mohamad.syahiran25@gmail.com>
2023-07-24 00:44:55 +0800

    Insert file

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Countries from './components/Countries';
+import Country from './components/Country';
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Countries />} />
+          <Route path="/countries/:name" element={<Country />} />
+        </Routes>
+      </>
+    </Router>
+  );
+}
+
+export default App;
